c25dbdf5e03823b091b0e8f1e4d8ea90dc221014
Jovana <jjovana314@gmail.com>
2020-09-03 11:52:13 +0200

    created function validate_schema

--- web/helper.py
+++ web/helper.py
@@ -1,35 +1,41 @@
-all_inner_keys = ["kind", "channel_id", "video_id", "total_results", "results_page"]
-inner_values_type = [str, str, str, int, int]
-zipped_keys_types = list(zip(all_inner_keys, inner_values_type))
+from jsonschema import validate
+from jsonschema.exceptions import ValidateError
+from json import loads, dumps
 
 
-def validate_type(value: object, type_: type, min_=None, max_=None) -> None:
-    if not isinstance(value, type_):
-        raise ValueError(f"{value} must be instance of {type_.__name__}")
-    
-    # if type_ is str:
-    #     if min_ is not None and max_ is not None:
-    #         if len(value) < min_:
-    #             raise ValueError(f"Length of {value} cannot be less than {min_}")
-    #         if len(value) > max_:
-    #             raise ValueError(f"Length of {value} cannot be greather than {max_}")
+class InvalidSchemaError(Exception):
+    """ Raised if schema is not valid. """
 
-    # if type_ is dict:
-    #     keys = value.keys()
-    #     for key in keys:
-    #         if key not in all_inner_keys:
-    #             raise KeyError(f"'{key}' is not valid")
-    #     for key, type_inner in zipped_keys_types:
-    #         try:
-    #             current_value = value[key]
-    #         except KeyError:
-    #             pass
-    #         validate_type(current_value, type_inner)
 
-    # if type_ is int:
-    #     if min_ is not None and max_ is not None:
-    #         if value < min_:
-    #             raise ValueError(f"{value} cannot be less than {min_}")
-    #         if value > max_:
-    #             raise ValueError(f"{value} cannot be greather than {max_}")
-                
+schema_errors = [
+    "Failed validating 'type' in schema",
+    "Failed validating 'required' in schema",
+    "Failed validating 'minLength' in schema",
+    "Failed validating 'maxLength' in schema",
+    "Failed validating 'minimum' in schema",
+    "Failed validating 'maximum' in schema",
+    "Failed validating 'minItems' in schema",
+    "Failed validating 'maxItems' in schema"
+]
+
+INVALID_SCHEMA = 451
+
+
+def validate_schema(schema: dict, data: dict):
+    """ Schemma validation.
+
+    Args:
+        schema (dict): valid dictionary
+        data (dict): dictionary for validation
+
+    Raises:
+        InvalidSchemaError: if data (schema) is not valid
+    """
+    # data = dumps(data)
+    try:
+        validate(data, schema)
+    except ValidationError as ex:
+        ex_str = str(ex)
+        for message in schema_errors:
+            if message in ex_str:
+                raise InvalidSchemaError(message, INVALID_SCHEMA)
