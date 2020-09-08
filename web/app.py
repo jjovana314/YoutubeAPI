bffd3907c25e3f670d59f6b06ee26a31a491070f
Jovana <jjovana314@gmail.com>
2020-09-08 18:33:27 +0200

    reduced code in post method

--- web/app.py
+++ web/app.py
@@ -31,31 +31,13 @@ class Search(Resource):
         for key in valid_keys:
             data_outter[key] = data.get(key, None)
 
-        try:
-            helper.validate_schema(schema, data_outter)
-        except helper.InvalidSchemaError as ex:
-            return jsonify({"message": ex.args[0], "code": ex.args[1]})
-        outter_data = helper.caller(data, helper.send_data_values)
-        items_data = helper.caller(data, helper.send_items_values)
-
-        if isinstance(outter_data, KeyError) or isinstance(outter_data, ValueError):
-            return jsonify({"message": outter_data.args[0], "code": outter_data.args[1]})
-        if isinstance(items_data, KeyError) or isinstance(outter_data, ValueError):
-            return jsonify({"message": items_data.args[0], "code": items_data.args[1]})
-
         results = data["result"]
-        try:
-            helper.validate_schema(schema, results)
-        except helper.InvalidSchemaError as ex:
-            return jsonify({"message": ex.args[0], "code": ex.args[1]})
-        data_result = helper.caller(results, helper.send_data_values)
-        items_result = helper.caller(results, helper.send_items_values)
 
-        if isinstance(data_result, KeyError) or isinstance(data_result, ValueError):
-            return jsonify({"message": data_result.args[0], "code": data_result.args[1]})
-
-        if isinstance(items_result, KeyError) or isinstance(items_result, ValueError):
-            return jsonify({"message": items_result.args[0], "code": items_result.args[1]})
+        all_data_valid = helper.validation(data_outter, results, schema)
+        if isinstance(all_data_valid, tuple):
+            return jsonify({"message": all_data_valid[0], "code": all_data_valid[1]})
+        # all_data_valid is list
+        outter_data, data_result, items_data, items_result = all_data_valid
 
         search.insert(
             {
